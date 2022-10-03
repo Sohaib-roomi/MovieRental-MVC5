89a77079e7cd1c897259b9fc5974780a161701ff
Sohaib-roomi <roomi.sohaib4@gmail.com>
2022-10-03 17:45:20 +0500

    first-commit

---
+++ MovieRentalApp/Controllers/Accounts/MyAccountsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRentalApp.Controllers.Accounts
+{
+    public class MyAccountsController : Controller
+    {
+        // GET: MyAccounts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
